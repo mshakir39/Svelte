4cbf869fb3aa797a4bbd85a3dd244cde8202eae5
Muzamil Shakir <mshakir39@gmail.com>
2023-01-15 16:03:35 +0500

    navBar added

---
+++ src/components/Navbar/navbar.svelte
@@ -0,0 +1,73 @@
+<script>
+	import { Collapse, Navbar, NavbarToggler, NavbarBrand, Nav, NavItem, NavLink } from 'sveltestrap';
+
+	let isOpen = false;
+
+	// @ts-ignore
+	function handleUpdate(event) {
+		isOpen = event.detail.isOpen;
+	}
+</script>
+
+<svelte:head>
+	<link
+		rel="stylesheet"
+		href="https://cdn.jsdelivr.net/npm/bootstrap@5.2.1/dist/css/bootstrap.min.css"
+	/>
+</svelte:head>
+
+<Navbar color="dark" light expand="md">
+	<NavbarBrand href="/"
+		><img
+			src="/images/brandLogo.svg"
+			alt=""
+			style="margin-left:60px;margin-right:175px;"
+		/></NavbarBrand
+	>
+	<NavbarToggler on:click={() => (isOpen = !isOpen)} />
+	<Collapse {isOpen} navbar expand="md" on:update={handleUpdate}>
+		<Nav class="" navbar style="color:#fffff">
+			<NavItem>
+				<span>
+					<NavLink href="#/">Products</NavLink>
+				</span>
+			</NavItem>
+			<NavItem>
+				<span>
+					<NavLink href="#">Technologies</NavLink>
+				</span>
+			</NavItem>
+			<NavItem>
+				<span>
+					<NavLink href="#">Resources</NavLink>
+				</span>
+			</NavItem>
+			<NavItem style="margin-right:428px">
+				<span>
+					<NavLink href="#">Blog</NavLink>
+				</span>
+			</NavItem>
+			<NavItem>
+				<span>
+					<NavLink href="#">GITHUB</NavLink>
+				</span>
+			</NavItem>
+			<NavItem>
+				<span>
+					<NavLink href="#" style="color:white;white-space:nowrap">CONTACT SALES</NavLink>
+				</span>
+			</NavItem>
+		</Nav>
+	</Collapse>
+</Navbar>
+
+<style global>
+	span :global(a) {
+		color: #787d8e;
+		font-size: 16px;
+	}
+
+	span :global(a):hover {
+		color: white;
+	}
+</style>
--- src/routes/+page.svelte
+++ src/routes/+page.svelte
@@ -1,14 +1,16 @@
 <script lang="ts">
 	import Card from '../components/Card/index.svelte';
-	const details = {
-		title: 'Universal key value',
-		description:
-			'Universal DBMS for Docs • Graphs • Blobs • Vectors × in Local • Remote × RocksDB • LevelDB • UMem • UDisk × from C • C++ • Python • Java • GoLang',
-		status: 'public',
-		lang: 'C++',
-		stars: 64,
-		shares: 5
-	};
+	import Navbar from '../components/Navbar/navbar.svelte';
+
+	// const details = {
+	// 	title: 'Universal key value',
+	// 	description:
+	// 		'Universal DBMS for Docs • Graphs • Blobs • Vectors × in Local • Remote × RocksDB • LevelDB • UMem • UDisk × from C • C++ • Python • Java • GoLang',
+	// 	status: 'public',
+	// 	lang: 'C++',
+	// 	stars: 64,
+	// 	shares: 5
+	// };
 </script>
 
-<Card {details} />
+<Navbar />
